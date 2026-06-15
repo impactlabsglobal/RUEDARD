933386a29e3d2f53d73a4fbc8dc7b5f0c1e5ce91
mrbatista <Mr.Robertbatistajerez@gmail.com>
2026-06-15 16:08:22 -0400

    Forzar actualizacion del menu movil

--- sw.js
+++ sw.js
@@ -85,9 +85,9 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
+    )).then(() => self.clients.claim()).then(() => self.clients.matchAll({ type: 'window' }))
+      .then((clients) => Promise.all(clients.map((client) => client.navigate(client.url))))
   );
-  self.clients.claim();
 });
 
 async function patchedHtml(response) {
